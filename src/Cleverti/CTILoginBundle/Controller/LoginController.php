5a597fbef3466ceebe4373abcbbbadb6db2a0ca0
Pedro Resende <pedroresende@mail.resende.biz>
2014-04-03 07:01:11 +0100

    - Play around with the form and the 'POST' method

--- src/Cleverti/CTILoginBundle/Controller/LoginController.php
+++ src/Cleverti/CTILoginBundle/Controller/LoginController.php
@@ -3,11 +3,19 @@
 namespace Cleverti\CTILoginBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class LoginController extends Controller
 {
-    public function indexAction()
+    public function indexAction( Request $request)
     {
+        if( $request->getMethod() == 'POST' ) {
+            $login = $request->request->get('Login');
+            $password = $request->request->get('Password');
+            var_dump($login);
+            var_dump($password);
+        }
+        
         return $this->render('ClevertiCTILoginBundle::login_form.html.twig');
     }
     
---
+++ src/Cleverti/CTILoginBundle/Entity/User.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Cleverti\CTILoginBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Cleverti\CTILoginBundle\Entity\UserRepository")
+ */
+class User
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return User
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
