3ac13ad82af5fc2e0c1eb413b70bb9640229184c
Pedro Resende <pedroresende@mail.resende.biz>
2014-06-11 08:45:31 +0100

    - Generated the doctrine form for the Language entity

---
+++ src/Xvolutions/AdminBundle/Form/LanguageType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Xvolutions\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class LanguageType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('language')
+            ->add('code')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Xvolutions\AdminBundle\Entity\Language'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'xvolutions_adminbundle_language';
+    }
+}
