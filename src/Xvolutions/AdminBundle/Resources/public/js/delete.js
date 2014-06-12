5fa081014c25eff0d59cf0dce90a75b5f59b2cd8
Pedro Resende <pedroresende@mail.resende.biz>
2014-06-12 15:53:27 +0100

    - Updated the delete.js script to take into account the action of removing more than one

--- src/Xvolutions/AdminBundle/Resources/public/js/delete.js
+++ src/Xvolutions/AdminBundle/Resources/public/js/delete.js
@@ -10,8 +10,17 @@ $(document).ready(function()
             url: url,
             async: true,
             cache: false,
-            success: function()
+            success: function( result )
             {
+                var text= result;
+                $("#status").fadeIn(
+                        'slow',
+                        function()
+                        {
+                            $("#status").text(text);
+                            $("#status").css("display","block");
+                        }
+                );
                 $("."+tr).fadeOut(
                         'slow', 
                         function()
@@ -19,6 +28,71 @@ $(document).ready(function()
                                 $("."+tr).remove();
                             }
                 );
+            },
+            statusCode: {
+                400: function( result ) {
+                    var text= result;
+                    $("#status").text(text);
+                    $("#status").fadeIn(
+                            'slow',
+                            function()
+                            {
+                                $("#status").css("display","block");
+                            }
+                    );
+                }
+            }
+        }
+        );
+    }
+    );
+
+    $('a.btn.btn-danger.btn-sm').click(function()
+    {
+        var url = $(this).attr('name');
+        var tmp_tr = url.substring(url.indexOf("[")+1,url.indexOf("]"));
+        var tmp_tr = tmp_tr.replace(/"/g,'');
+        var tr = tmp_tr.split(",");
+        $.ajax(
+        {
+            type: "DELETE",
+            url: url,
+            async: true,
+            cache: false,
+            success: function( result )
+            {
+                var text= result;
+                $("#status").fadeIn(
+                        'slow',
+                        function()
+                        {
+                            $("#status").text(text);
+                            $("#status").css("display","block");
+                        }
+                );
+                for ( var i in tr) {
+                    $(".tr"+tr[i]).fadeOut(
+                        'slow', 
+                        function()
+                            {
+                                $(".tr"+tr[i]).remove();
+                            }
+                    );
+                }
+                
+            },
+            statusCode: {
+                400: function( result ) {
+                    var text= result;
+                    $("#status").text(text);
+                    $("#status").fadeIn(
+                            'slow',
+                            function()
+                            {
+                                $("#status").css("display","block");
+                            }
+                    );
+                }
             }
         }
         );
