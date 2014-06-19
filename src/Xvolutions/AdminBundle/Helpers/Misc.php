3c71d85b6a34b7675641f6f03ef46dd64615542a
Pedro Resende <pedroresende@mail.resende.biz>
2014-06-19 15:27:45 +0100

    - Removed default since it was generating the wrong size

--- src/Xvolutions/AdminBundle/Helpers/Misc.php
+++ src/Xvolutions/AdminBundle/Helpers/Misc.php
@@ -14,12 +14,11 @@ class Misc {
      * This funciton is responsible for fetching the gravatar of the given e-mail
      * 
      * @param type $email Email of the user
-     * @param type $default The default image
      * @param type $size The intended image size
      * @return url of the gravatar
      */
-    public function fetchGravatar($email, $default = 'http://www.gravatar.com/avatar/', $size = 20) {
-        return "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
+    public function fetchGravatar($email, $size = 20) {
+        return "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
     }
 
 }
