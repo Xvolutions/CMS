3cadf5c5168a4e3956fb5146198a50fab82df791
Pedro Resende <pedroresende@mail.resende.biz>
2014-06-22 22:05:08 +0100

    - Added Pagination Class and tests

---
+++ src/Xvolutions/AdminBundle/Helpers/Pagination.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Xvolutions\AdminBundle\Helpers;
+
+/**
+ * Description of Pagination
+ *
+ * @author Pedro Resende <pedroresende@mail.resende.biz>
+ * @date 22/06/2014
+ * 
+ */
+class Pagination {
+    /*
+     * The current page
+     */
+
+    private $current_page = null;
+
+    /*
+     * The totla number of pages
+     */
+    private $total_pages = null;
+
+    /*
+     * The number of pages to show since page one and from the the last one
+     */
+    private $boundaries = null;
+
+    /*
+     * The number of pages to show around the current page
+     */
+    private $around = null;
+
+    /*
+     * The array of pages to be displayed
+     */
+    private $pages = null;
+
+    /*
+     * Class constructor
+     */
+
+    public function __construct($current_page, $total_pages, $boundaries, $around) {
+        $this->current_page = $current_page;
+        $this->total_pages = $total_pages;
+        $this->boundaries = $boundaries;
+        $this->around = $around;
+        $this->bootstrapPages();
+    }
+
+    public function displayPagination() {
+        $this->validation();
+
+        $this->beginning();
+        $this->current();
+        $this->ending();
+
+        $output = null;
+        $previous = null;
+        for ($i = 1; $i < $this->total_pages + 1; $i++) {
+            if ($this->pages[$i - 1] == 1) {
+                $output .= $i . ' ';
+                $actual = 1;
+            } else {
+                if ($previous != 0) {
+                    $output .= '... ';
+                }
+                $actual = 0;
+            }
+            $previous = $actual;
+        }
+
+        return substr($output, 0, strlen($output)-1);;
+    }
+
+    /*
+     * Function responsible to fill the pages array with zeros (0)
+     */
+
+    private function bootstrapPages() {
+        $this->pages = array_fill(0, $this->total_pages, 0);
+    }
+
+    /*
+     * Function responsible to validate the variables
+     */
+
+    private function validation() {
+        if (is_numeric($this->current_page) && is_numeric($this->total_pages) && is_numeric($this->boundaries) && is_numeric($this->around)) {
+            if ($this->current_page < 1 || $this->current_page > $this->total_pages) {
+                $this->current_page = 1;
+            }
+        } else {
+            throw new Exception('InvalidArgumentException');
+        }
+    }
+
+    /*
+     * Function responsible to display the first page with the boundaries
+     */
+
+    private function beginning() {
+        if ($this->boundaries > 1) {
+            for ($i = 1; $i < $this->boundaries + 1; $i ++) {
+                $this->pages[$i - 1] = 1;
+            }
+        } else {
+            $this->pages[0] = 1;
+        }
+    }
+
+    /*
+     * Function reponsible to displau the current page with the arround ones
+     */
+
+    private function current() {
+        if ($this->current_page - $this->around < 1) {
+            $start = 1;
+        } else {
+            $start = $this->current_page - $this->around;
+        }
+        if ($this->current_page + $this->around > $this->total_pages + 1) {
+            $end = $this->total_pages + 1;
+        } else {
+            $end = $this->current_page + $this->around + 1;
+        }
+
+        for ($i = $start; $i < $end; $i++) {
+            $this->pages[$i - 1] = 1;
+        }
+    }
+
+    /*
+     * Function responsible to display the last page with boundaries
+     */
+
+    private function ending() {
+        if ($this->boundaries > 1) {
+            $first = $this->total_pages - $this->boundaries + 1;
+            for ($i = $first; $i < $this->total_pages + 1; $i ++) {
+                $this->pages[$i - 1] = 1;
+            }
+        } else {
+            $this->pages[$this->total_pages - 1] = 1;
+        }
+    }
+
+}
---
+++ src/Xvolutions/AdminBundle/Tests/Helpers/PaginationTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Xvolutions\AdminBundle\Tests\Helpers;
+
+use Xvolutions\AdminBundle\Helpers\Pagination;
+
+/**
+ * Description of PaginationTest
+ *
+ * @author Pedro Resende <pedroresende@mail.resende.biz>
+ */
+class PaginationTest extends \PHPUnit_Framework_TestCase {
+
+    public function testDisplayPagination1() {
+        $pagination = new Pagination( 4, 5, 1, 0);
+        $result = $pagination->displayPagination();
+
+        $this->assertEquals( '1 ... 4 5',$result);
+    }
+
+    public function testDisplayPagination2() {
+        $pagination = new Pagination( 4, 10, 2, 2);
+        $result = $pagination->displayPagination();
+
+        $this->assertEquals( '1 2 3 4 5 6 ... 9 10',$result);
+    }
+
+    public function testDisplayPagination3() {
+        $pagination = new Pagination( 4, 100, 2, 2);
+        $result = $pagination->displayPagination();
+
+        $this->assertEquals( '1 2 3 4 5 6 ... 99 100',$result);
+    }
+
+    public function testDisplayPagination4() {
+        $pagination = new Pagination( -1, 100, 2, 4);
+        $result = $pagination->displayPagination();
+
+        $this->assertEquals( '1 2 3 4 5 ... 99 100',$result);
+    }
+
+    public function testDisplayPagination5() {
+        $pagination = new Pagination( 100, 100, 2, 4);
+        $result = $pagination->displayPagination();
+
+        $this->assertEquals( '1 2 ... 96 97 98 99 100',$result);
+    }
+
+    public function testDisplayPagination6() {
+        $pagination = new Pagination( 50, 100, 2, 4);
+        $result = $pagination->displayPagination();
+        
+        $this->assertEquals( '1 2 ... 46 47 48 49 50 51 52 53 54 ... 99 100',$result);
+    }
+
+    public function testDisplayPagination7() {
+        $pagination = new Pagination( 50, 100, 101, 101);
+        $result = $pagination->displayPagination();
+        
+        $this->assertEquals( '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100',$result);
+    }
+}
