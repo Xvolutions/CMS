2bb0c5fbd5167472c2521fd59605c8fffb43995c
Pedro Resende <pedroresende@mail.resende.biz>
2014-06-25 17:05:15 +0100

    - Refractor the Admin Controller and moved the templates

--- src/Xvolutions/AdminBundle/Controller/AdminController.php
+++ src/Xvolutions/AdminBundle/Controller/AdminController.php
@@ -21,7 +21,7 @@ class AdminController extends Controller {
         $this->verifyaccess();
 
         return $this->render(
-            'XvolutionsAdminBundle:pages:main.html.twig');
+            'XvolutionsAdminBundle::main.html.twig');
     }
 
     /**
@@ -31,7 +31,7 @@ class AdminController extends Controller {
      */
     public function setupAction() {
         $this->verifyaccess();
-        return $this->render('XvolutionsAdminBundle:pages:setup.html.twig');
+        return $this->render('XvolutionsAdminBundle::setup.html.twig');
     }
 
     /**
