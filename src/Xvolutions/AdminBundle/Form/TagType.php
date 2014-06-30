9ade6a58a92bb6db0707e85950c62d46c9f92348
Pedro Resende <pedroresende@mail.resende.biz>
2014-06-30 09:05:31 +0100

    - Renamed the TagType

--- src/Xvolutions/AdminBundle/Form/TagType.php
+++ src/Xvolutions/AdminBundle/Form/TagType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class TagsType extends AbstractType
+class TagType extends AbstractType
 {
         /**
      * @param FormBuilderInterface $builder
@@ -25,7 +25,7 @@ class TagsType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Xvolutions\AdminBundle\Entity\Tags'
+            'data_class' => 'Xvolutions\AdminBundle\Entity\Tag'
         ));
     }
 
@@ -34,6 +34,6 @@ class TagsType extends AbstractType
      */
     public function getName()
     {
-        return 'xvolutions_adminbundle_tags';
+        return 'xvolutions_adminbundle_tag';
     }
 }
