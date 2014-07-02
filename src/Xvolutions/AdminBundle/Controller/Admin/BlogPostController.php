a1e62e4cc02aa5bca7843f8ce3c48c552a1e0af2
Pedro Resende <pedroresende@mail.resende.biz>
2014-07-02 15:11:46 +0100

    - Implement the BlogPostController - Updated the BlogPostType to match the needs - Updated the posts.html.twig template to display the Blog Posts

--- src/Xvolutions/AdminBundle/Controller/Admin/BlogPostController.php
+++ src/Xvolutions/AdminBundle/Controller/Admin/BlogPostController.php
@@ -9,7 +9,6 @@ use Xvolutions\AdminBundle\Entity\BlogPost;
 use Xvolutions\AdminBundle\Form\BlogPostType;
 use Symfony\Component\Security\Core\Exception;
 
-
 /**
  * Description of BlogPostController
  *
@@ -25,7 +24,8 @@ class BlogPostController extends AdminController
      * @param \Symfony\Component\HttpFoundation\Request $request
      * @return type the templates for adding a new blog post
      */
-    public function addBlogPostsAction(Request $request) {
+    public function addBlogPostsAction(Request $request)
+    {
         parent::verifyaccess();
 
         $blogPost = new BlogPost();
@@ -34,6 +34,8 @@ class BlogPostController extends AdminController
         $form = $this->createForm($blogPostType, $blogPost)
                 ->add('Criar', 'submit')
         ;
+
+        $form->get('author')->setData(parent::getUsername());
 
         $form->handleRequest($request);
 
@@ -71,24 +73,31 @@ class BlogPostController extends AdminController
      * @param type $id the id of an existing blog post
      * @return type the template for editing a blog post
      */
-    public function editBlogPostsAction(Request $request, $id) {
+    public function editBlogPostsAction(Request $request, $id)
+    {
         parent::verifyaccess();
 
         $blogPostType = new BlogPostType();
 
-        $blogPost = $this->getDoctrine()->getRepository( 'XvolutionsAdminBundle:BlogPost' )->find( $id );
+        $blogPost = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:BlogPost')->find($id);
 
-        $form = $this->createForm( $blogPostType, $blogPost )
-                ->add( 'Guardar', 'submit' )
+        $form = $this->createForm($blogPostType, $blogPost)
+                ->add(
+                        'author', null, array(
+                    'label' => 'Autor',
+                    'disabled' => true,
+                    'data' => parent::getUsername()
+                        )
+                )
+                ->add('Guardar', 'submit')
         ;
 
         $status = null;
         $error = null;
 
-        $form->handleRequest( $request );
+        $form->handleRequest($request);
 
-        if ( $form->isValid() )
-        {
+        if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->flush();
             $status = 'Artigo actualizado com sucesso';
@@ -96,7 +105,7 @@ class BlogPostController extends AdminController
             $blogPostList = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:BlogPost')->findAll();
 
             return $this->render('XvolutionsAdminBundle:blog:post/posts.html.twig', array(
-                        'title' => 'Categorias',
+                        'title' => 'Artigos',
                         'blogPostList' => $blogPostList,
                         'status' => $status,
                         'error' => $error,
@@ -105,9 +114,104 @@ class BlogPostController extends AdminController
 
         return $this->render('XvolutionsAdminBundle:blog:post/add_posts.html.twig', array(
                     'form' => $form->createView(),
-                    'title' => 'Editar uma Categoria',
+                    'title' => 'Editar um Artigo',
                     'status' => $status,
                     'error' => $error
         ));
     }
+
+    /**
+     * Controller responsible to show the blog post for and handling the form
+     * submission and the database insertion
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return type the template of the blogPosts
+     */
+    public function blogPostsAction($option = NULL, $id = NULL)
+    {
+        parent::verifyaccess();
+
+        $status = null;
+        $error = null;
+        switch ($option) {
+            case 'remove': {
+                    $this->removeBlogPost($id, $status, $error);
+                    break;
+                }
+            case 'removeselected': {
+                    $ids = json_decode($id);
+                    $this->removeSelectedBlogPosts($ids, $status, $error);
+                    break;
+                }
+        }
+
+        if ($error != null) {
+            return new Response($error, Response::HTTP_BAD_REQUEST);
+        }
+        if ($status != null) {
+            return new Response($status, Response::HTTP_OK);
+        }
+
+        $blogPostList = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:BlogPost')->findAll();
+
+        return $this->render('XvolutionsAdminBundle:blog:post/posts.html.twig', array(
+                    'title' => 'Artigos',
+                    'blogPostList' => $blogPostList,
+                    'status' => $status,
+                    'error' => $error,
+        ));
+    }
+
+    /**
+     * This is function is repsonsible to remove a blog post
+     * 
+     * @param type $id the id of the blog post to be removed
+     * @return string with the information message
+     */
+    private function removeBlogPost($id, &$status, &$error)
+    {
+        try {
+            $em = $this->getDoctrine()->getManager();
+            $blogPost = $em->getRepository('XvolutionsAdminBundle:BlogPost')->find($id);
+            if ($blogPost != 'empty') {
+                $em->remove($blogPost);
+                $em->flush();
+                $status = 'Artigo removido com sucesso';
+            } else {
+                $error = "Erro ao remover o artigo";
+            }
+        } catch (Exception $ex) {
+            $error = "Erro $ex ao remover o artigo";
+        }
+    }
+
+    /**
+     * This function is responsible to remove a list of blog posts
+     * 
+     * @param type $ids array containing the id's of the blog posts to be removed
+     * @return string with the message
+     */
+    private function removeSelectedBlogPosts($ids, &$status, &$error)
+    {
+        try {
+            $em = $this->getDoctrine()->getManager();
+            foreach ($ids as $id)
+            {
+                $blogPost = $em->getRepository('XvolutionsAdminBundle:BlogPost')->find($id);
+                if ($blogPost != 'empty') {
+                    $em->remove($blogPost);
+                    $em->flush();
+                    $status = 'Artigo removido com sucesso';
+                } else {
+                    $error = "Erro ao remover o(s) artigo(s)";
+                }
+            }
+            if ($error == null) {
+                $status = 'Artigos removidos com sucesso';
+            }
+        } catch (Exception $ex) {
+            $error = "Erro $ex ao remover o(s) artigo(s)";
+        }
+    }
+
 }
