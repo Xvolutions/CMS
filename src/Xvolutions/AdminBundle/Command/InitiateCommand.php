801f25acfec2e9383624242223e44db0d0ea27fb
Pedro Resende <pedroresende@mail.resende.biz>
2014-07-03 13:32:46 +0100

    - Fix Exception by \ErrorException and fix PSR-1 coding standards

--- src/Xvolutions/AdminBundle/Command/InitiateCommand.php
+++ src/Xvolutions/AdminBundle/Command/InitiateCommand.php
@@ -13,6 +13,7 @@ use Xvolutions\AdminBundle\Entity\User;
 use Xvolutions\AdminBundle\Entity\Role;
 use Xvolutions\AdminBundle\Entity\Category;
 use Xvolutions\AdminBundle\Entity\Tag;
+use Symfony\Component\Debug\ErrorHandler;
 
 /**
  * Description of InitiateCommand
@@ -25,8 +26,8 @@ class InitiateCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-                ->setName( 'xvolutions:admin:initiate' )
-                ->setDescription( 'This command will populate the Database' )
+                ->setName('xvolutions:admin:initiate')
+                ->setDescription('This command will populate the Database')
                 ->addArgument('username', InputArgument::REQUIRED, 'Nome do Utilizador')
                 ->addArgument('name', InputArgument::REQUIRED, 'Nome completo')
                 ->addArgument('password', InputArgument::REQUIRED, 'Palavra chave')
@@ -34,195 +35,204 @@ class InitiateCommand extends ContainerAwareCommand
         ;
     }
 
-    protected function execute( InputInterface $input, OutputInterface $output )
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
+        ErrorHandler::register();
         $username = $input->getArgument('username');
         $name = $input->getArgument('name');
         $password = $input->getArgument('password');
         $email = $input->getArgument('email');
-        $this->initiateRoles( $output );
-        $this->initiateUsers( $output, $username, $name, $password, $email );
-        $this->initiateSections( $output );
-        $this->initiateCategories( $output );
-        $this->initiateTags( $output );
-        $this->initiateLanguages( $output );
-        $this->initiatePages( $output );
+        $this->initiateRoles($output);
+        $this->initiateUsers($output, $username, $name, $password, $email);
+        $this->initiateSections($output);
+        $this->initiateCategories($output);
+        $this->initiateTags($output);
+        $this->initiateLanguages($output);
+        $this->initiatePages($output);
     }
 
-    private function initiateSections( &$output )
+    private function initiateSections(&$output)
     {
         try {
-            $em = $this->getContainer()->get( 'doctrine' )->getManager( 'default' );
+            $em = $this->getContainer()->get('doctrine')->getManager('default');
 
-            $sections = array( 'Pública', 'Privada' );
-            foreach ( $sections as $s ) {
+            $sections = array('Pública', 'Privada');
+            foreach ($sections as $s)
+            {
                 $section = new Section();
-                $section->setSection( $s );
-                $em->persist( $section );
-                $em->flush( $section );
+                $section->setSection($s);
+                $em->persist($section);
+                $em->flush($section);
             }
-            $output->writeln( 'Secções Adicionadas' );
-        } catch ( Exception $ex ) {
-            $output->writeln( "Erro ao adicionar as secções base -> $ex" );
+            $output->writeln('Secções Adicionadas');
+        } catch (\ErrorException $ex) {
+            $output->writeln("Erro ao adicionar as secções base -> $ex");
         }
     }
 
-    private function initiateCategories( &$output )
+    private function initiateCategories(&$output)
     {
         try {
-            $em = $this->getContainer()->get( 'doctrine' )->getManager( 'default' );
+            $em = $this->getContainer()->get('doctrine')->getManager('default');
 
-            $categories = array( 'Geral', 'Desenvolvimento', 'Web' );
-            foreach ( $categories as $c ) {
+            $categories = array('Geral', 'Desenvolvimento', 'Web');
+            foreach ($categories as $c)
+            {
                 $category = new Category();
-                $category->setCategory( $c );
-                $em->persist( $category );
-                $em->flush( $category );
+                $category->setCategory($c);
+                $em->persist($category);
+                $em->flush($category);
             }
-            $output->writeln( 'Categorias Adicionadas' );
-        } catch ( Exception $ex ) {
-            $output->writeln( "Erro ao adicionar as categorias base -> $ex" );
+            $output->writeln('Categorias Adicionadas');
+        } catch (\ErrorException $ex) {
+            $output->writeln("Erro ao adicionar as categorias base -> $ex");
         }
     }
 
-    private function initiateTags( &$output )
+    private function initiateTags(&$output)
     {
         try {
-            $em = $this->getContainer()->get( 'doctrine' )->getManager( 'default' );
+            $em = $this->getContainer()->get('doctrine')->getManager('default');
 
-            $tags = array( 'dev', 'web' );
-            foreach ( $tags as $t ) {
+            $tags = array('dev', 'web');
+            foreach ($tags as $t)
+            {
                 $tag = new Tag();
-                $tag->setTag( $t );
-                $em->persist( $tag );
-                $em->flush( $tag );
+                $tag->setTag($t);
+                $em->persist($tag);
+                $em->flush($tag);
             }
-            $output->writeln( 'Palavras-Chave Adicionadas' );
-        } catch ( Exception $ex ) {
-            $output->writeln( "Erro ao adicionar as palavras-chave base -> $ex" );
+            $output->writeln('Palavras-Chave Adicionadas');
+        } catch (\ErrorException $ex) {
+            $output->writeln("Erro ao adicionar as palavras-chave base -> $ex");
         }
     }
 
-    private function initiatePages( &$output )
+    private function initiatePages(&$output)
     {
         try {
-            $em = $this->getContainer()->get( 'doctrine' )->getManager( 'default' );
-            $sectionList = $this->getContainer()->get( 'doctrine' )->getRepository( 'XvolutionsAdminBundle:Section' )->find( 0 );
-            $languageList = $this->getContainer()->get( 'doctrine' )->getRepository( 'XvolutionsAdminBundle:Language' )->find( 0 );
-            $pages = array( '-----' );
-            foreach ( $pages as $p ) {
+            $em = $this->getContainer()->get('doctrine')->getManager('default');
+            $sectionList = $this->getContainer()->get('doctrine')->getRepository('XvolutionsAdminBundle:Section')->find(0);
+            $languageList = $this->getContainer()->get('doctrine')->getRepository('XvolutionsAdminBundle:Language')->find(0);
+            $pages = array('-----');
+            foreach ($pages as $p)
+            {
                 $page = new Page();
-                $page->setTitle( $p );
-                $page->setUrl( $p );
-                $page->setIdparent( 0 );
-                $page->setIdlanguage( $languageList[0] );
-                $page->setIdsection( $sectionList[0] );
-                $datetime = new \DateTime( 'now' );
-                $page->setDate( $datetime );
-                $em->persist( $page );
-                $em->flush( $page );
+                $page->setTitle($p);
+                $page->setUrl($p);
+                $page->setIdparent(0);
+                $page->setIdlanguage($languageList[0]);
+                $page->setIdsection($sectionList[0]);
+                $datetime = new \DateTime('now');
+                $page->setDate($datetime);
+                $em->persist($page);
+                $em->flush($page);
             }
-            $output->writeln( 'Páginas Adicionadas' );
-        } catch ( Exception $ex ) {
-            $output->writeln( "Erro ao adicionar as páginas base -> $ex" );
+            $output->writeln('Páginas Adicionadas');
+        } catch (\ErrorException $ex) {
+            $output->writeln("Erro ao adicionar as páginas base -> $ex");
         }
     }
 
-    private function initiateLanguages( &$output )
+    private function initiateLanguages(&$output)
     {
         try {
-            $em = $this->getContainer()->get( 'doctrine' )->getManager( 'default' );
+            $em = $this->getContainer()->get('doctrine')->getManager('default');
 
-            $languages = array( 
+            $languages = array(
                 array(
-                    'English', 
+                    'English',
                     'en_GB'
-                ), 
+                ),
                 array(
-                    'Português', 
+                    'Português',
                     'pt-PT'
                 )
             );
-            
-            foreach( $languages as $l) {
+
+            foreach ($languages as $l)
+            {
                 $language = new Language();
-                $language->setLanguage( $l[0] );
-                $language->setCode( $l[1] );
-                $em->persist( $language );
-                $em->flush( $language );
+                $language->setLanguage($l[0]);
+                $language->setCode($l[1]);
+                $em->persist($language);
+                $em->flush($language);
             }
-            $output->writeln( 'Ídiomas Adicionados' );
-        } catch ( Exception $ex ) {
-            $output->writeln( "Erro ao adicionar os idiomas base -> $ex" );
+            $output->writeln('Ídiomas Adicionados');
+        } catch (\ErrorException $ex) {
+            $output->writeln("Erro ao adicionar os idiomas base -> $ex");
         }
     }
 
-    private function initiateRoles( &$output )
+    private function initiateRoles(&$output)
     {
         try {
-            $em = $this->getContainer()->get( 'doctrine' )->getManager( 'default' );
+            $em = $this->getContainer()->get('doctrine')->getManager('default');
 
-            $roles = array( 
+            $roles = array(
                 array(
-                    'Admin', 
+                    'Admin',
                     'ROLE_ADMIN'
-                ), 
+                ),
                 array(
-                    'User', 
+                    'User',
                     'ROLE_USER'
                 )
             );
-            
-            foreach( $roles as $r) {
+
+            foreach ($roles as $r)
+            {
                 $role = new Role();
-                $role->setName( $r[0] );
-                $role->setRole( $r[1] );
-                $em->persist( $role );
-                $em->flush( $role );
+                $role->setName($r[0]);
+                $role->setRole($r[1]);
+                $em->persist($role);
+                $em->flush($role);
             }
-            $output->writeln( 'Grupos Adicionados' );
-        } catch ( Exception $ex ) {
-            $output->writeln( "Erro ao adicionar os grupos base -> $ex" );
+            $output->writeln('Grupos Adicionados');
+        } catch (\ErrorException $ex) {
+            $output->writeln("Erro ao adicionar os grupos base -> $ex");
         }
     }
 
-    private function initiateUsers( &$output, $username, $name, $password, $email)
+    private function initiateUsers(&$output, $username, $name, $password, $email)
     {
         try {
-            $role = $this->getContainer()->get( 'doctrine' )->getRepository( 'XvolutionsAdminBundle:Role' )->findAll();
+            $role = $this->getContainer()->get('doctrine')->getRepository('XvolutionsAdminBundle:Role')->findAll();
             $role0 = $role[0];
-            $users = array( 
+            $users = array(
                 array(
                     $username,
                     $name,
                     $password,
                     $email,
                     '1',
-                    array( $role0 )
-                ), 
+                    array($role0)
+                ),
             );
-            $em = $this->getContainer()->get( 'doctrine' )->getManager( 'default' );
-            foreach( $users as $u) {
+            $em = $this->getContainer()->get('doctrine')->getManager('default');
+            foreach ($users as $u)
+            {
                 $user = new User();
-                $user->setUsername( $u[0] );
-                $user->setName( $u[1] );
+                $user->setUsername($u[0]);
+                $user->setName($u[1]);
                 $salt = md5(time());
                 $user->setSalt($salt);
 
                 $factory = $this->getContainer()->get('security.encoder_factory');
-                $encoder = $factory->getEncoder( $user );
+                $encoder = $factory->getEncoder($user);
                 $password = $encoder->encodePassword($u[2], $user->getSalt());
                 $user->setPassword($password);
 
-                $user->setEmail( $u[3] );
-                $user->setIsactive( $u[4] );
-                $user->setRoles( $u[5] );
-                $em->persist( $user );
-                $em->flush( $user );
+                $user->setEmail($u[3]);
+                $user->setIsactive($u[4]);
+                $user->setRoles($u[5]);
+                $em->persist($user);
+                $em->flush($user);
             }
-            $output->writeln( 'Utilizadores Adicionados' );
-        } catch ( Exception $ex ) {
-            $output->writeln( "Erro ao adicionar os utilizadores base -> $ex" );
+            $output->writeln('Utilizadores Adicionados');
+        } catch (\ErrorException $ex) {
+            $output->writeln("Erro ao adicionar os utilizadores base -> $ex");
         }
     }
+
 }
