1da94f434e10df79f5d2fddd2a92391b2e9d29f6
Pedro Resende <pedroresende@mail.resende.biz>
2014-07-09 21:14:06 +0100

    - PSR-0 on Pagination class

--- src/Xvolutions/AdminBundle/Helpers/Pagination.php
+++ src/Xvolutions/AdminBundle/Helpers/Pagination.php
@@ -11,8 +11,7 @@ use Symfony\Component\Debug\ErrorHandler;
  * @date 22/06/2014
  * 
  */
-class Pagination
-{
+class Pagination {
     /*
      * The current page
      */
@@ -42,9 +41,7 @@ class Pagination
     /*
      * Class constructor
      */
-
-    public function __construct($current_page, $total_pages, $boundaries, $around)
-    {
+    public function __construct($current_page, $total_pages, $boundaries, $around) {
         $this->current_page = $current_page;
         $this->total_pages = $total_pages;
         $this->boundaries = $boundaries;
@@ -55,9 +52,7 @@ class Pagination
     /*
      * Function responsible to display the page numbers
      */
-
-    public function displayPagination()
-    {
+    public function displayPagination() {
         $this->validation();
 
         $this->beginning();
@@ -66,8 +61,7 @@ class Pagination
 
         $output = null;
         $previous = null;
-        for ($i = 1; $i < $this->total_pages + 1; $i++)
-        {
+        for ($i = 1; $i < $this->total_pages + 1; $i++) {
             if ($this->pages[$i - 1] == 1) {
                 $output .= $i . ' ';
                 $actual = 1;
@@ -87,17 +81,14 @@ class Pagination
      * Function responsible to fill the pages array with zeros (0)
      */
 
-    private function bootstrapPages()
-    {
+    private function bootstrapPages() {
         $this->pages = array_fill(0, $this->total_pages, 0);
     }
 
     /*
      * Function responsible to validate the variables
      */
-
-    private function validation()
-    {
+    private function validation() {
         if (is_numeric($this->current_page) && is_numeric($this->total_pages) && is_numeric($this->boundaries) && is_numeric($this->around)) {
             if ($this->current_page < 1 || $this->current_page > $this->total_pages) {
                 $this->current_page = 1;
@@ -110,12 +101,9 @@ class Pagination
     /*
      * Function responsible to display the first page with the boundaries
      */
-
-    private function beginning()
-    {
+    private function beginning() {
         if ($this->boundaries > 1) {
-            for ($i = 1; $i < $this->boundaries + 1; $i ++)
-            {
+            for ($i = 1; $i < $this->boundaries + 1; $i ++) {
                 $this->pages[$i - 1] = 1;
             }
         } else {
@@ -126,9 +114,7 @@ class Pagination
     /*
      * Function reponsible to displau the current page with the arround ones
      */
-
-    private function current()
-    {
+    private function current() {
         if ($this->current_page - $this->around < 1) {
             $start = 1;
         } else {
@@ -140,8 +126,7 @@ class Pagination
             $end = $this->current_page + $this->around + 1;
         }
 
-        for ($i = $start; $i < $end; $i++)
-        {
+        for ($i = $start; $i < $end; $i++) {
             $this->pages[$i - 1] = 1;
         }
     }
@@ -149,13 +134,10 @@ class Pagination
     /*
      * Function responsible to display the last page with boundaries
      */
-
-    private function ending()
-    {
+    private function ending() {
         if ($this->boundaries > 1) {
             $first = $this->total_pages - $this->boundaries + 1;
-            for ($i = $first; $i < $this->total_pages + 1; $i ++)
-            {
+            for ($i = $first; $i < $this->total_pages + 1; $i ++) {
                 $this->pages[$i - 1] = 1;
             }
         } else {
