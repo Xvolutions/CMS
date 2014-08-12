e11822c9ecaee739bc8b70c4cd8d6c58cd9f12c7
pedroresende <pedroresende@mail.resende.biz>
2014-08-12 17:08:10 +0100

    - Started re-designing login test

--- src/Xvolutions/AdminBundle/Tests/Controller/SecurityControllerTest.php
+++ src/Xvolutions/AdminBundle/Tests/Controller/SecurityControllerTest.php
@@ -3,6 +3,8 @@
 namespace Xvolutions\AdminBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 /**
  * Description of SecurityControllerTest
@@ -11,12 +13,20 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class SecurityControllerTest extends WebTestCase {
 
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
     public function testlogin() {
-        $client = static::createClient();
+        $this->logIn();
 
-        $crawler = $client->request('GET', '/admin/');
+        $crawler = $this->client->request('GET', '/admin/backoffice');
 
-        $this->assertTrue($crawler->filter('html:contains("Consola de Gestão")')->count() > 0);
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Consola de Gestão")')->count());
     }
 
     public function testrecover() {
@@ -25,5 +35,18 @@ class SecurityControllerTest extends WebTestCase {
         $crawler = $client->request('GET', '/admin/recover');
 
         $this->assertTrue($crawler->filter('html:contains("Consola de Gestão")')->count() > 0);
+    }
+
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewall = 'secured_area';
+        $token = new UsernamePasswordToken('admin', 'adminpass', $firewall, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
     }
 }
