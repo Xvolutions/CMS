59878ac83d42cb695bcff3f48be595c6f06fa86a
Pedro Resende <pedroresende@mail.resende.biz>
2015-02-01 12:13:08 +0000

    Fix travis

--- bin/travis/prepare_environment.sh
+++ bin/travis/prepare_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Preparing parameters.yml"
-cp xvolutionscms/config/parameters.yml.dist xvolutonscoms/config/parameters.yml
-sed -i 's/database_user:     root/database_user:     symfony/g'
-sed -i 's/database_password: ~/database_password: symfony/g'
+cp xvolutionscms/config/parameters.yml.dist xvolutionscms/config/parameters.yml
+sed -i 's/database_user:     root/database_user:     symfony/g' xvolutionscms/config/parameters.yml
+sed -i 's/database_password: ~/database_password: symfony/g' xvolutionscms/config/parameters.yml
 
 echo "Install dependencies through composer"
 composer install
