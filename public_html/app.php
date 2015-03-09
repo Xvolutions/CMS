a8fe9979ed8ac1748d0f95e6daad3dc1d2ca3dd7
Pedro Resende <pedroresende@mail.resende.biz>
2015-03-09 02:45:55 +0000

    - Enabled AppCache wrapper http://symfony.com/doc/current/book/http_cache.html#symfony-reverse-proxy

--- public_html/app.php
+++ public_html/app.php
@@ -15,11 +15,11 @@ $apcLoader->register(true);
 
 
 require_once __DIR__.'/../xvolutionscms/AppKernel.php';
-//require_once __DIR__.'/../xvolutionscms/AppCache.php';
+require_once __DIR__.'/../xvolutionscms/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
