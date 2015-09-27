db2890536a7ad79ee7d2a4124a51d780f53cce4d
Pedro Resende <pedroresende@mail.resende.biz>
2015-09-27 03:08:29 +0100

    - Implement missing controllers and remaining methods related with the menu

--- src/Xvolutions/AdminBundle/Controller/Admin/MenuController.php
+++ src/Xvolutions/AdminBundle/Controller/Admin/MenuController.php
@@ -5,6 +5,7 @@ namespace Xvolutions\AdminBundle\Controller\Admin;
 use Xvolutions\AdminBundle\Controller\AdminController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Xvolutions\AdminBundle\Entity\Menu;
 
 /**
  * Description of MenuController
@@ -14,7 +15,13 @@ use Symfony\Component\HttpFoundation\Response;
 class MenuController extends AdminController
 {
 
-    public function menuAction($option = null, $id = null, $status = null, $error = null)
+    /**
+     * 
+     * @param type $status
+     * @param type $error
+     * @return Response
+     */
+    public function menuAction($status = null, $error = null)
     {
         parent::verifyaccess();
 
@@ -25,13 +32,119 @@ class MenuController extends AdminController
             return new Response($status, Response::HTTP_OK);
         }
 
-        $AliasList = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:Alias')->findAll();
+        $menuItems = $this->getMenuItems();
+        $pages = $this->getPages($menuItems);
 
         return $this->render('XvolutionsAdminBundle:menu:menu.html.twig', array(
                     'title' => 'Menus',
-                    'AliasList' => $AliasList,
+                    'menuItems' => $menuItems,
+                    'pages' => $pages,
                     'status' => $status,
                     'error' => $error
         ));
     }
+
+    /**
+     * This method is responsible for storing the menu items position and updating
+     * them if theirs position changes
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function updateAction(Request $request)
+    {
+        parent::verifyaccess();
+
+        $menuOrder = $request->request->get('id');
+        $em = $this->getDoctrine()->getManager();
+        $this->clearMenu($em);
+        if ($menuOrder != null) {
+            foreach ($menuOrder as $key => $pageId) {
+                $reOrder = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:Menu')->findBy(array('page' => $pageId));
+
+                if (isset($reOrder[0])) {
+                    if ($reOrder[0]->getPosition() != $key) {
+                        $reOrder[0]->setPosition($key);
+
+                        $em->persist($reOrder[0]);
+                        $em->flush();
+                    }
+                } else {
+                    $this->addToMenu($pageId, $key, $em);
+                }
+            }
+        }
+
+        return new Response('Updated', Response::HTTP_OK);
+    }
+
+    /**
+     * This method is responsible for getting all the menu items aka Pages
+     * 
+     * @return array Array containing the page id and the page title
+     */
+    private function getMenuItems()
+    {
+        $menu = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:Menu')->findBy([], array('position' => 'ASC'));
+        $menuItems = null;
+        foreach ($menu as $item) {
+            $menuItems[$item->getPage()->getId()] = ['id' => $item->getPage()->getId(), 'title' => $item->getPage()->getTitle()];
+        }
+
+        return $menuItems;
+    }
+
+    /**
+     * This method is responsible for returning the list of pages that aren't
+     * in the menu
+     * 
+     * @param array $menuItems all pages in the menu
+     * @return array Array containing the page id and the page title
+     */
+    private function getPages($menuItems)
+    {
+        $aliasList = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:Alias')->findBy(array('type' => '1'));
+        $pages = null;
+        foreach ($aliasList as $alias) {
+            $page = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:Page')->findBy(array('id_alias' => $alias->getId()));
+            if (!isset($menuItems[$page[0]->getId()])) {
+                $pages[] = ['id' => $page[0]->getId(), 'title' => $page[0]->getTitle()];
+            }
+        }
+
+        return $pages;
+    }
+
+    /**
+     * This method is responsible for removing all entries from the menu
+     * 
+     * @param doctrine $em Doctrine
+     */
+    private function clearMenu($em)
+    {
+        $menu = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:Menu')->findAll();
+        foreach ($menu as $item) {
+            $em->remove($item);
+            $em->flush();
+        }
+    }
+
+    /**
+     * This method is responsible for storing a page into the menu
+     * 
+     * @param integer $pageId The id of the page
+     * @param integer $key The foreach index
+     * @param doctrine $em Doctrine
+     */
+    private function addToMenu($pageId, $key, $em)
+    {
+        $menu = new Menu();
+        $page = $this->getDoctrine()->getRepository('XvolutionsAdminBundle:Page')->find($pageId);
+        $menu->setPage($page);
+        $menu->setPosition($key);
+
+        $em->persist($menu);
+        $em->flush();
+    }
+
 }
